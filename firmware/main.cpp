21f80bda8a5a80f71083f5c979184c355459855f
Alexander Entinger <cto@lxrobotics.com>
2016-03-28 21:17:33 +0200

    Adding two services initPPM and setPPM for controlling the ppm generator in the arduino firmware

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,28 +1,75 @@
-#include <ros.h>
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
 
 #include <Arduino.h>
 
+#include <ros.h>
+
+#include <ardumote/initPPM.h>
+#include <ardumote/setPPM.h>
+
 #include "PPMGenerator.h"
 
-static uint8_t const NUM_PPM_CHANNELS = 5;
+/**************************************************************************************
+ * GLOBAL CONSTANTS
+ **************************************************************************************/
+
 static uint16_t const INITIAL_CHANNEL_VALUE_US = 1500;
+
+/**************************************************************************************
+ * PROTOTYPES
+ **************************************************************************************/
+
+void initPPMCallbackFunction(ardumote::initPPM::Request const &req, ardumote::initPPM::Response &res);
+void setPPMCallbackFunction(ardumote::setPPM::Request const &req, ardumote::setPPM::Response &res);
+
+/**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
 
 ros::NodeHandle node_handle;
 
+ros::ServiceServer<ardumote::initPPM::Request, ardumote::initPPM::Response> service_server_init_ppm("initPPM", &initPPMCallbackFunction);
+ros::ServiceServer<ardumote::setPPM::Request, ardumote::setPPM::Response> service_server_set_ppm("setPPM", &setPPMCallbackFunction);
+
+/**************************************************************************************
+ * ARDUINO FRAMEWORK FUNCTIONS
+ **************************************************************************************/
+
 void setup() 
 {
-  PPMGenerator::begin(NUM_PPM_CHANNELS);
-
-  PPMGenerator::setPulseWidthUs(0, INITIAL_CHANNEL_VALUE_US);
-  PPMGenerator::setPulseWidthUs(1, INITIAL_CHANNEL_VALUE_US);
-  PPMGenerator::setPulseWidthUs(2, INITIAL_CHANNEL_VALUE_US);
-  PPMGenerator::setPulseWidthUs(3, INITIAL_CHANNEL_VALUE_US);
-  PPMGenerator::setPulseWidthUs(4, INITIAL_CHANNEL_VALUE_US);
-
   node_handle.initNode();
+
+  node_handle.advertiseService(service_server_init_ppm);
+  node_handle.advertiseService(service_server_set_ppm);
 }
 
 void loop() 
 {
+  node_handle.spinOnce();
+}
 
+/**************************************************************************************
+ * OUR FUNCTIONS
+ **************************************************************************************/
+
+void initPPMCallbackFunction(ardumote::initPPM::Request const &req, ardumote::initPPM::Response &res)
+{
+  bool success = PPMGenerator::begin(req.num_channels);
+
+  for(uint8_t i = 0; i < req.num_channels; i++)
+  {
+    if(!PPMGenerator::setPulseWidthUs(i, INITIAL_CHANNEL_VALUE_US))
+    {
+      success = false;
+    }
+  }
+
+  res.result = success;
+}
+
+void setPPMCallbackFunction(ardumote::setPPM::Request const &req, ardumote::setPPM::Response &res)
+{
+  res.result = PPMGenerator::setPulseWidthUs(req.channel, req.pulse_duration_us);
 }
