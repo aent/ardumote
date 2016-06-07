3c2ab9a292fea9b63eeb996b2c2600a2661492a4
Alexander Entinger <cto@lxrobotics.com>
2016-06-07 11:09:24 +0200

    Finished rebuilding ardumote from using services to using topics

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -21,6 +21,8 @@ static uint8_t  const DEFAULT_NUM_OF_CHANNELS  = 5;
  * PROTOTYPES
  **************************************************************************************/
 
+void retrieveParameters(uint8_t *num_channels);
+void initPPMGenerator(uint8_t const num_channels);
 void PPMCallbackFunction(const ardumote::PPM &ppm_msg);
 
 /**************************************************************************************
@@ -37,6 +39,8 @@ ros::Subscriber<ardumote::PPM> ppm_subscriber("/ppm", &PPMCallbackFunction);
 
 void setup() 
 {
+  /* Setup the connection to the ROS */
+
   node_handle.getHardware()->setBaud(115200);
   node_handle.initNode();
 
@@ -45,18 +49,14 @@ void setup()
     node_handle.spinOnce();
   }
 
-  int num_channels = 0;
-  bool const success = node_handle.getParam("num_channels", &num_channels);
-  if(!success)
-  {
-    num_channels = DEFAULT_NUM_OF_CHANNELS;
-  }
+  /* Initialize the PPM generator library */
 
-  PPMGenerator::begin((uint8_t)(num_channels));
-  for(uint8_t c = 0; c < num_channels; c++)
-  {
-    PPMGenerator::setPulseWidthUs(c, INITIAL_CHANNEL_VALUE_US);
-  }
+  uint8_t num_channels = 0;
+  retrieveParameters(&num_channels);
+
+  initPPMGenerator(num_channels);
+
+  /* Subscribe to the topic */
 
   node_handle.subscribe(ppm_subscriber);
 }
@@ -70,7 +70,34 @@ void loop()
  * OUR FUNCTIONS
  **************************************************************************************/
 
+void retrieveParameters(uint8_t *num_channels)
+{
+  int int_num_channels = 0;
+  bool const success = node_handle.getParam("ardumote/num_channels", &int_num_channels);
+  if(success)
+  {
+    *num_channels = int_num_channels;
+  }
+  else
+  {
+    *num_channels = DEFAULT_NUM_OF_CHANNELS;
+  }
+}
+
+void initPPMGenerator(uint8_t const num_channels)
+{
+  PPMGenerator::begin(num_channels);
+  for(uint8_t c = 0; c < num_channels; c++)
+  {
+    PPMGenerator::setPulseWidthUs(c, INITIAL_CHANNEL_VALUE_US);
+  }
+}
+
 void PPMCallbackFunction(const ardumote::PPM &ppm_msg)
 {
-  PPMGenerator::setPulseWidthUs(ppm_msg.channel, ppm_msg.pulse_duration_us);
+  bool const success = PPMGenerator::setPulseWidthUs(ppm_msg.channel, ppm_msg.pulse_duration_us);
+  if(!success)
+  {
+    node_handle.logerror("Could not set desired pulse width");
+  }
 }
